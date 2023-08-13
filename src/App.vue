05011e6c57932ddc03f2842b2de564a8d26bdd9e
luanafernanda <lfsg.22@outlook.com>
2023-08-13 15:53:04 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+import Calculadora from './components/Calculadora.vue'
+
+<template>
+  <div id="app">
+    <Calculadora />
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+    Calculadora
+  }
+}
+</script>
+
+<style>
+  body {
+    background-color: #D1C4E9;
+  }
+
+  #app {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 90vh;
+  }
+</style>
---
+++ src/components/Calculadora.vue
@@ -0,0 +1,99 @@
+<template>
+
+    <div class="container">
+        <h1>Calculadora Aritmética</h1>
+        <div class="calculadora">
+            <input type="number" v-model="numero1" @input="calculate" />
+            <input type="number" v-model="numero2" @input="calculate" />
+
+            <select v-model="operacao" @change="calculate">
+                <option value="adicao">+</option>
+                <option value="subtracao">-</option>
+                <option value="multiplicacao">*</option>
+                <option value="divisao">/</option>
+            </select>
+        <div>Resultado: {{ resultado }}</div>
+    </div>
+    
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+            numero1: 0,
+            numero2: 0,
+            operacao: 'adicao',
+            resultado: 0
+            };
+        },
+        methods: {
+            calculate() {
+                switch (this.operacao) {
+                    case 'adicao':
+                        this.resultado = parseFloat(this.numero1) + parseFloat(this.numero2);
+                        break;
+                    case 'subtracao':
+                        this.resultado = parseFloat(this.numero1) - parseFloat(this.numero2);
+                        break;
+                    case 'multiplicacao':
+                        this.resultado = parseFloat(this.numero1) * parseFloat(this.numero2);
+                        break;
+                    case 'divisao':
+                        if (this.numero2 != 0) {
+                        this.resultado = parseFloat(this.numero1) / parseFloat(this.numero2);
+                        } else {
+                        this.resultado = 'teste';
+                        }
+                        break;
+                }
+            }
+        },
+        mounted() {
+            this.calculate();
+        }
+    }
+</script>
+
+<style>
+    .container, h1 {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    h1 {
+        text-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    }
+
+    .calculadora {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 15px; 
+        padding: 20px;
+        border: 1px solid black;
+        border-radius: 10px;
+        width: 300px; 
+       
+    }
+
+    input, select {
+        width: 100%;
+        padding: 10px;
+        border: 1px solid black;
+        border-radius: 5px;
+        font-size: 18px;
+    }
+
+    select {
+        width: 50%;
+        text-align-last: center; 
+    }  
+
+    .calculadora, input, select {
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    }
+
+</style>
